eb793d69cf61c066ac5c9e10704132313f689a66
KingAndrew801 <111556315+KingAndrew801@users.noreply.github.com>
2023-01-17 06:58:03 -0700

    massive changes

--- app.py
+++ app.py
@@ -1,50 +1,83 @@
 from constants import PLAYERS, TEAMS
 from teams import Team
 
-def clean_data(data):
-    squeaky = []
-    for player in data.copy():
-        player['guardians'] = player['guardians'].split(" and ")
-        if player['experience'] == "YES":
-            player['experience'] = True
-        elif player['experience'] == "NO":
-            player['experience'] = False
-        player['height'] = int(player['height'].split()[0])
-        squeaky.append(player)
-    return squeaky
+class Team:
 
-def pick_teams(data, players):
-    teams = []
-    for t in data:
-        teams.append(Team(t))
-    print(len(teams))
-    xp = []
-    noob = []
-    for player in players:
-        if player['experience'] == True:
-            xp.append(player)
-        else:
-            noob.append(player)
-    for t in teams:
-        xpplayersnum = int(len(xp))
-        for i in range(0, int(xpplayersnum / len(teams))):
-            print(f'looping; {xp}')
-            t.players.append(xp[0])
-            del xp[0]
-        print(f'finished team : {t.players}')
+    def __init__(self, teamname):
+        self.name = teamname
+        self.number_of_players = 0
+        self.number_of_expplayers = 0
+        self.number_of_noobs = 0
+        self.players = []
+        self.avgheight = None
 
-def counter(players):
-    xp = []
-    noob = []
-    for p in players:
-        if p['experience'] == False:
-            noob.append(p)
-        else:
-            xp.append(p)
-    print(len(xp))
-    print(len(noob))
+    def __repr__(self):
+        return f'{self.name}'
+
+class Game:
+
+    def __init__(self):
+        self.teams = []
+
+    def team_maker(self, tdata):
+        newteams = []
+        for team in tdata:
+            newteams.append(Team(team))
+        return newteams
+
+
+    def clean_data(self, pdata):
+        squeaky = []
+        data = pdata.copy()
+        for player in data:
+            player['guardians'] = player['guardians'].split(" and ")
+            if player['experience'] == "YES":
+                player['experience'] = True
+            elif player['experience'] == "NO":
+                player['experience'] = False
+            player['height'] = int(player['height'].split()[0])
+            squeaky.append(player)
+        return squeaky
+
+    def pick_teams(self, pdata):
+        xp = []
+        noobs = []
+        for p in pdata:
+            if p['experience']  == True:
+                xp.append(p)
+            elif p['experience'] == False:
+                noobs.append(p)
+        noobshare = int(len(noobs) / len(self.teams))
+        xpshare = int(len(xp) / len(self.teams))
+
+        for t in self.teams:
+            pindex = 0
+            for p in xp:
+                print(p)
+                rangeindex = 0
+                for _ in range(xpshare):
+                    print(f'range index: {rangeindex}')
+                    rangeindex += 1
+                    if p['experience'] == True:
+                        print(p)
+                        print(xp)
+                        t.players.append(xp[pindex])
+                        print(f'xp[pindex]: {xp[pindex]}')
+                        del xp[pindex]
+                        print(f'########### this is self.teams:{t}')
+                pindex += 1
+
+
+
+    def run_it(self, pdata, tdata):
+        self.teams = self.team_maker(tdata)
+        self.pick_teams(pdata.copy())
+
+
 
 
 if __name__ == '__main__':
-    wenis = pick_teams(TEAMS, clean_data(PLAYERS))
-    print(wenis)
+    game = Game()
+    game.run_it(game.clean_data(PLAYERS), TEAMS)
+
+
